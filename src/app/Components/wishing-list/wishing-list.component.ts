adf4bb09687e83f278a7da13867e854b7c0755de
EhabAhmed <e.ahmed2684@gmail.com>
2025-05-03 22:46:00 +0300

    init

---
+++ src/app/Components/wishing-list/wishing-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wishing-list',
+  imports: [],
+  templateUrl: './wishing-list.component.html',
+  styleUrl: './wishing-list.component.css'
+})
+export class WishingListComponent {
+
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { authGuard } from './Guards/auth.guard';
+import { ErrorComponent } from './Components/error/error.component';
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent, title: 'Home'},
+    {path: 'products', component: ProductsComponent, title: 'Products'},
+    {path: 'products/:id', component: ProductDetailsComponent, title: 'Product Details'},
+    {path: 'cart', loadChildren:()=>import('./Components/cart/cart.component').then(c => c.CartComponent) , title: 'Cart', canActivate: [authGuard]},
+    {path: 'Wishing', loadChildren: () => import('./Components/wishing-list/wishing-list.component').then(m => m.WishingListComponent), title: 'Wishing', canActivate: [authGuard] },
+    {path: 'checkout', loadChildren: () => import('./Components/checkout/checkout.component').then(m => m.CheckoutComponent), title: 'Checkout', canActivate: [authGuard] },
+    {path: 'login', loadChildren: () => import('./Components/login/login.component').then(m => m.LoginComponent), title: 'Login' },
+    {path: 'register', loadChildren: () => import('./Components/register/register.component').then(m => m.RegisterComponent), title: 'Register' },
+    {path: 'profile', loadChildren: () => import('./Components/profile/profile.component').then(m => m.ProfileComponent), title: 'Profile', canActivate: [authGuard] },
+    {path: '**', component: ErrorComponent, title: 'Error' }
+];
