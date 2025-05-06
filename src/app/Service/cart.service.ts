0f5d71185e72e988740a0197c66f058ca6f1561b
alyaa999 <alyaamamoon999@gmail.com>
2025-05-06 03:19:46 +0300

    add-cart

---
+++ src/app/Service/cart.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/enviroment';
+import { Observable } from 'rxjs/internal/Observable';
+
+import { Cart, CartProduct } from '../Interfaces/Cart/Cart.models';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+   
+  private apiUrl = `${environment.apiBaseUrl}/Cart/viewCartProducts`; 
+  constructor( private http : HttpClient) { 
+
+  }
+    getCart(): Observable<Cart> {
+          return this.http.get<Cart>(`${this.apiUrl}`); 
+  }
+
+}
