20c26e009522c5387e1d6c7184eb008d6142ed04
rahmaelsayedddd <119257518+rahmaelsayedddd@users.noreply.github.com>
2025-05-07 16:44:08 +0300

    added filter and products components

--- src/app/Components/products/products.component.ts
+++ src/app/Components/products/products.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  standalone: true,  // Required if using standalone
+  imports: [RouterModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+  public products = [
+    { id: 1, img: "1", name: "Classic Trench Coat", price: 200, category: "women" },
+    { id: 2, img: "2", name: "Front Pocket Jumper", price: 500, category: "women" }
+  ];
 
+  changeHeartIcon(event: MouseEvent): void {
+    const target = event.currentTarget as HTMLElement;
+    const img = target.querySelector('img');
+
+    if (img) {
+      img.src = 'Icons/icon-heart-02.png';
+    }
+  }
+
+  resetHeartIcon(event: MouseEvent): void {
+    const target = event.currentTarget as HTMLElement;
+    const img = target.querySelector('img');
+
+    if (img) {
+      img.src = 'Icons/icon-heart-01.png';
+    }
+  }
+
+  @Output() quickViewClicked = new EventEmitter<number>();
+  @Output() wishlistToggled = new EventEmitter<number>();
+
+  openQuickView(productId: number) {
+    this.quickViewClicked.emit(productId);
+  }
+
+  toggleWishlist(productId: number) {
+    this.wishlistToggled.emit(productId);
+  }
+
+  AddToWishlist(){
+    
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,15 @@ import { ProductDetailsComponent } from './Components/product-details/product-de
 import { CartComponent } from './Components/cart/cart.component';
 import { authGuard } from './Guards/auth.guard';
 import { ErrorComponent } from './Components/error/error.component';
+import { FilterComponent } from './Components/filter/filter.component';
+import { ShopComponent } from './Components/shop/shop.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent, title: 'Home'},
     {path: 'products', component: ProductsComponent, title: 'Products'},
+    {path: 'filter', component: FilterComponent, title: 'Filter'},
+    {path: 'shop', component: ShopComponent, title: 'Shop'},
     {path: 'products/:id', component: ProductDetailsComponent, title: 'Product Details'},
     {path: 'cart', loadChildren:()=>import('./Components/cart/cart.component').then(c => c.CartComponent) , title: 'Cart', canActivate: [authGuard]},
     {path: 'Wishing', loadChildren: () => import('./Components/wishing-list/wishing-list.component').then(m => m.WishingListComponent), title: 'Wishing', canActivate: [authGuard] },
