b6677aafc9a1184838ace933e06bd94da3d445cc
alyaa999 <alyaamamoon999@gmail.com>
2025-05-12 19:26:48 +0300

    add-checkout-order-thankyou

--- src/app/Service/cart.service.ts
+++ src/app/Service/cart.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/enviroment';
 import { Observable } from 'rxjs/internal/Observable';
 
-import { Cart, CartProduct } from '../Interfaces/Cart/Cart.models';
 import { HttpClient } from '@angular/common/http';
+import { CustomerBasket } from '../Interfaces/Cart/Cart.models';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-   
-  private apiUrl = `${environment.apiBaseUrl}/Cart/viewCartProducts`; 
+  private apiUrl = `${environment.apiBaseUrl}Baskets/basket`;
   constructor( private http : HttpClient) { 
 
   }
-    getCart(): Observable<Cart> {
-          return this.http.get<Cart>(`${this.apiUrl}`); 
+    getCart(): Observable<CustomerBasket> {
+      return this.http.get<CustomerBasket>(`${this.apiUrl}`); 
   }
 
+  
 }
