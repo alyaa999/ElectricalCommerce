df41d62965f97975ba8277a13e367b1407ac24f4
EhabAhmed <e.ahmed2684@gmail.com>
2025-05-16 16:10:43 +0300

    Edit Base Api For Deployment

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:44390/api',
-  stripKey:"sk_test_51RNkjuLXzNqSsQMTxqev63GLMvI4sMKMynVwLYL6jIqxq9ivhKsorQyA1hxck4bOljuzJNweVuNr7bgsAQNzEZyy00nJL4ReLw",
-  
+  apiBaseUrl: 'https://electromart.runasp.net/api',
+  stripKey: "sk_test_51RNkjuLXzNqSsQMTxqev63GLMvI4sMKMynVwLYL6jIqxq9ivhKsorQyA1hxck4bOljuzJNweVuNr7bgsAQNzEZyy00nJL4ReLw",
+
 };
